3f337dfa013bfdeddd0b6636d9d7bec6cc62c208
Wellerosn Assis <wellerson18assis@outlook.com>
2026-07-28 18:33:09 -0300

    refactor: remove arquivos obsoletos do FutGG no backend e limpa o Controller

--- src/main/java/com/sbc/sbc_calculator_backend/dto/SquadRequestDto.java
+++ src/main/java/com/sbc/sbc_calculator_backend/dto/SquadRequestDto.java
@@ -1,9 +1,22 @@
 package com.sbc.sbc_calculator_backend.dto;
 
-import lombok.Data;
 import java.util.List;
 
-@Data
 public class SquadRequestDto {
     private List<Integer> ratings;
+
+    public SquadRequestDto() {
+    }
+
+    public SquadRequestDto(List<Integer> ratings) {
+        this.ratings = ratings;
+    }
+
+    public List<Integer> getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(List<Integer> ratings) {
+        this.ratings = ratings;
+    }
 }
